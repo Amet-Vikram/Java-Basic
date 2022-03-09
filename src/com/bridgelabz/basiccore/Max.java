c9f7b18b527cc53dc329a4d26a2b7af414cf9a29
Amet-Vikram <amet1197@gmail.com>
2022-03-09 15:54:46 +0530

    Added Basic Core Problems

---
+++ src/com/bridgelabz/basiccore/Max.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.basiccore;
+
+public class Max {
+
+    static int maxOfThree(int a, int b, int c){
+        if (a > b && a > c){
+            return a;
+        }
+        else if (b > c){
+            return b;
+        }
+        else return c;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(maxOfThree(30, 12, 10));
+    }
+}
---
+++ src/com/bridgelabz/basiccore/VowelOrConsonant.java
@@ -0,0 +1,24 @@
+package com.bridgelabz.basiccore;
+
+import java.util.Scanner;
+
+public class VowelOrConsonant {
+
+    static Scanner keyboard = new Scanner(System.in);
+
+    static boolean isVowel(String letter){
+        char[] check = letter.toCharArray();
+        char[] vowel = "aeiouAEIOU".toCharArray();
+        for (char c : vowel) {
+            if (check[0] == c) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        System.out.println("Enter the letter: ");
+        String a = keyboard.next();
+        System.out.println(isVowel(a));
+    }
+}
