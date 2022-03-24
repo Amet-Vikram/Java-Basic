be1cc3b3a28ab63afba656eab637a86384270e78
Amet-Vikram <amet1197@gmail.com>
2022-03-24 16:51:26 +0530

    Generics Max Test using Float Objects

--- src/com/bridgelabz/generics/MaxText.java
+++ src/com/bridgelabz/generics/MaxText.java
@@ -1,26 +1,26 @@
 package com.bridgelabz.generics;
 
-public class MaxText<Integer extends Comparable<Integer>> {
-    Integer a, b, c;
+public class MaxText<Double extends Comparable<Double>> {
+    Double a, b, c;
 
-    public MaxText(Integer a, Integer b, Integer c) {
+    public MaxText(Double a, Double b, Double c) {
         this.a = a;
         this.b = b;
         this.c = c;
     }
 
-    Integer maximum(){
-        if (a.compareTo(b) > 0 && a.compareTo(c) > 0)
+    Double maximum(){
+        if (a.compareTo(b) > 0.0 && a.compareTo(c) > 0.0)
             return a;
-        else if (b.compareTo(c) > 0)
+        else if (b.compareTo(c) > 0.0)
             return b;
         else return c;
     }
 
     public static void main(String[] args) {
-        MaxText test1 = new MaxText<>(10,5,3);
-        MaxText test2 = new MaxText<>(10,15,9);
-        MaxText test3 = new MaxText<>(16,5,19);
+        MaxText test1 = new MaxText(10.1,5.2,3.7);
+        MaxText test2 = new MaxText(7.5,15.3,9.3);
+        MaxText test3 = new MaxText(16.6,5.0,19.8);
 
         System.out.println("Result of TC 1: " + test1.maximum());
         System.out.println("Result of TC 2: " + test2.maximum());
