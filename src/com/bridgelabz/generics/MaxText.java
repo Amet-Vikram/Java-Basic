dcb6ca77d0ed0ae22efcdc941937bf9423e9e9dc
Amet-Vikram <amet1197@gmail.com>
2022-03-24 17:02:19 +0530

    Generics Max Test using String Objects

--- src/com/bridgelabz/generics/MaxText.java
+++ src/com/bridgelabz/generics/MaxText.java
@@ -1,26 +1,27 @@
 package com.bridgelabz.generics;
 
-public class MaxText<Double extends Comparable<Double>> {
-    Double a, b, c;
+public class MaxText<T extends Comparable<T>> {
+    T a, b, c;
 
-    public MaxText(Double a, Double b, Double c) {
+    public MaxText(T a, T b, T c) {
         this.a = a;
         this.b = b;
         this.c = c;
     }
 
-    Double maximum(){
-        if (a.compareTo(b) > 0.0 && a.compareTo(c) > 0.0)
+    T maximum(){
+        if (a.compareTo(b) > 0 && a.compareTo(c) > 0)
             return a;
-        else if (b.compareTo(c) > 0.0)
+        else if (b.compareTo(c) > 0)
             return b;
         else return c;
     }
 
     public static void main(String[] args) {
-        MaxText test1 = new MaxText(10.1,5.2,3.7);
-        MaxText test2 = new MaxText(7.5,15.3,9.3);
-        MaxText test3 = new MaxText(16.6,5.0,19.8);
+
+        MaxText<String> test1 = new MaxText<>("Peach","Apple","Banana");
+        MaxText<String> test2 = new MaxText<>("Apple","Peach","Banana");
+        MaxText<String> test3 = new MaxText<>("Apple","Banana","Peach");
 
         System.out.println("Result of TC 1: " + test1.maximum());
         System.out.println("Result of TC 2: " + test2.maximum());
